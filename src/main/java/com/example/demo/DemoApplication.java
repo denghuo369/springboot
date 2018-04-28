58786e7ba3578b1b8e5abbba262bbc03a74d52e8
zkk <Administrator@kkk>
2018-04-28 16:37:21 +0800

    简单配置

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,10 +12,15 @@ public class DemoApplication {
 	
 	
 	@RequestMapping("/")
-	String index() {
+	public String index() {
 		return "hello spring boot";
 	}
-
+	//访问/hello或者/hi任何一个地址，都会返回一样的结果
+    @RequestMapping(value = {"/hello","/hi"},method = RequestMethod.GET)
+	public String hello() {
+    	return "hello";
+    }
+    
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
